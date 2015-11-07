94cbca9111c462c669254b5e0f19e77ba816b1f6
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-07 17:53:17 -0500

    Configure devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Host should be set to actual host of application.
+  config.action_mailer.default_url_options = { http://www.trendingvirtual.com/ }
+
+  # For deployment to Heroku.
+  config.assets.initialize_on_precompile = false
 end
