807bf3d19dda3dc94b93c49c05b3fb45a65b4a14
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-12 16:56:31 -0500

    Temporarily remove validation on user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
 	default_scope -> { order(created_at: :desc) }
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
-	validates :user_id, presence: true
+	# validates :user_id, presence: true
 end
