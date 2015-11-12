a83b2e4a7a3c1d03d6bb9d1ae8a64fd94e134949
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-12 16:16:14 -0500

    Add descent and validations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
+	default_scope -> { order(created_at: :desc) }
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
+	validates :user_id, presence: true
 end
